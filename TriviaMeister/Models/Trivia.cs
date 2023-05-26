e323f61a8257e827726bb27d72e627b42df30621
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-05-27 03:44:59 +0800

    [Feat][Lex] Created Trivia models

---
+++ TriviaMeister/Models/Trivia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TriviaMeister.Models
+{
+    class Trivia
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<TriviaItem> Items { get; set; }
+    }
+}
---
+++ TriviaMeister/Models/TriviaItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TriviaMeister.Models
+{
+    class TriviaItem
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Prompt { get; set; }
+        public string Answer { get; set; }
+        public bool CaseSensitive { get; set; } = false;
+    }
+}
