bfe84f29e2af0195bfb42f6541ba0dba22197717
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-05-29 00:08:11 +0800

    [Feat][Lex] Created barebones TriviasPage

---
+++ TriviaMeister/Views/TriviasPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using TriviaMeister.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TriviaMeister.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TriviasPage : ContentPage
+	{
+		readonly TriviasViewModel _viewModel;
+
+		public TriviasPage ()
+		{
+			InitializeComponent ();
+
+			BindingContext = _viewModel = new TriviasViewModel();
+		}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+			_viewModel.OnAppearing();
+        }
+    }
+}
