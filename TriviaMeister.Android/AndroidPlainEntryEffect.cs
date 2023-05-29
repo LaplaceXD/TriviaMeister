31827698979a2739f28fba83b705618032c66e94
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-05-30 06:16:00 +0800

    [Feat][Lex] Created Effect to remove entry underlines

---
+++ TriviaMeister.Android/AndroidPlainEntryEffect.cs
@@ -0,0 +1,33 @@
+﻿using Android.Content.Res;
+using TriviaMeister.Droid;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ResolutionGroupName("PlainEntryGroup")]
+[assembly: ExportEffect(typeof(AndroidPlainEntryEffect), "PlainEntryEffect")]
+
+namespace TriviaMeister.Droid
+{
+    public class AndroidPlainEntryEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                if (Control != null)
+                {
+                    Control.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine("Unable to set property on attached control", ex.Message);
+            }
+        }
+
+        protected override void OnDetached() {}
+    }
+}
---
+++ TriviaMeister/Effects/PlainEntry.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+
+namespace TriviaMeister.Effects
+{
+    class PlainEntry : RoutingEffect
+    {
+        public PlainEntry() : base("PlainEntryGroup.PlainEntryEffect") { }
+    }
+}
