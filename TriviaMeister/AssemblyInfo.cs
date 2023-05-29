0c95ea4b59c3896a6d9cb3d4a1fb687ae31ea5bc
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-05-30 03:51:08 +0800

    [Feat][Lex] Added Base Color Palette and Fonts

--- TriviaMeister/AssemblyInfo.cs
+++ TriviaMeister/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Sansita-Bold.ttf", Alias = "SansitaBold")]
+[assembly: ExportFont("Sansita-Black.ttf", Alias = "SansitaBlack")]
+[assembly: ExportFont("Overpass.ttf", Alias = "Overpass")]
