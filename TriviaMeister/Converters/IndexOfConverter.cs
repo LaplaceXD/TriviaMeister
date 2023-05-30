796dbc53f283aba9440e57ce51c14749f96d7178
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-05-31 04:04:44 +0800

    [Fix][Lex] Made the numbering for TriviaItem questions functional

---
+++ TriviaMeister/Converters/IndexOfConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace TriviaMeister.Converters
+{
+    public class IndexOfConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null) return 0;
+            return ((ListView) parameter).ItemsSource.Cast<object>().ToList().IndexOf(value) + 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
