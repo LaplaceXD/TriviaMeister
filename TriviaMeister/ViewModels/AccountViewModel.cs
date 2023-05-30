55db00f02b82aac5882aa4210584f8c13f6780f9
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-05-31 06:45:40 +0800

    [Feat][Lex] Added AccountPage

---
+++ TriviaMeister/ViewModels/AccountViewModel.cs
@@ -0,0 +1,40 @@
+﻿using TriviaMeister.Views;
+using Xamarin.Forms;
+
+namespace TriviaMeister.ViewModels
+{
+    public class AccountViewModel : BaseViewModel
+    {
+        public string Name { get; private set; }
+        public Command SignOut { get; }
+
+        public AccountViewModel()
+        {
+            PageTitle = "Account";
+            LoadAccount();
+
+            SignOut = new Command(OnSignOut);
+        }
+
+        private async void LoadAccount()
+        {
+            var user = await AuthService.GetUser();
+            Name = user.Name;
+        }
+
+        private async void OnSignOut()
+        {
+            var isSuccessful = await AuthService.SignOutAsync();
+
+            if(isSuccessful)
+            {
+                await MessageService.ShowAsync("Sign Out", "You have signed out.", "Confirm");
+                App.Current.MainPage = new NavigationPage(new LoginPage());
+            }
+            else
+            {
+                await MessageService.ShowAsync("Error", "An error occurred while signing out.", "Confirm");
+            }
+        }
+    }
+}
---
+++ TriviaMeister/Views/AccountPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using TriviaMeister.ViewModels;
+
+namespace TriviaMeister.Views
+{
+    public partial class AccountPage : ContentPage
+    {
+        public AccountPage()
+        {
+            InitializeComponent();
+            BindingContext = new AccountViewModel();
+        }
+    }
+}
