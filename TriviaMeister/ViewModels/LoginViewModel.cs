90ac2d640f2627f643e944697d50786b904bf5ce
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-05-31 05:59:12 +0800

    [Feat][Lex] Styled and added login logic to login page

--- TriviaMeister/ViewModels/LoginViewModel.cs
+++ TriviaMeister/ViewModels/LoginViewModel.cs
@@ -8,6 +8,21 @@ namespace TriviaMeister.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
+        private string _email;
+        private string _password;
+
+        public string Email
+        {
+            get => _email;
+            set => SetProperty(ref _email, value);
+        }
+
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
         public Command LoginCommand { get; }
 
         public LoginViewModel()
@@ -17,6 +32,24 @@ namespace TriviaMeister.ViewModels
 
         private async void OnLoginClicked(object obj)
         {
+            if (string.IsNullOrEmpty(Email))
+            {
+                await MessageService.ShowAsync("Required", "Email is required.", "Ok");
+                return;
+            }
+            else if (string.IsNullOrEmpty(Password))
+            {
+                await MessageService.ShowAsync("Required", "Password is required.", "Ok");
+                return;
+            }
+
+            var isSuccessful = await AuthService.SignInAsync(Email, Password);
+            if(!isSuccessful)
+            {
+                await MessageService.ShowAsync("Invalid", "Invalid credentials.", "Ok");
+                return;
+            }
+
             // Fix this code to push to a new stack, instead of using the current navigation stack
             await Navigation.PushAsync(new AboutPage());
         }
